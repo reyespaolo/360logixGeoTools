6da773e6c4f0eee87a936f6f6d4b77db3ea7fa01
Paolo Reyes <reyes.paolo@360logix.com>
2016-10-24 12:13:38 +0800

    Added Reverse Geo for OSM, Google,and Bing, Also added Reverse Geo will pick provider base on given probability

--- src/geo-tools.js
+++ src/geo-tools.js
@@ -3,6 +3,226 @@ var url = require('url');
 
 var Logix360GeoTools = {
 
+	requestReverseGeo: function(myOptions,lat,lng,callback) {
+		if (lat.lat && lat.lng) {
+			callback = lng
+			lng = lat.lng
+			lat = lat.lat
+		};
+		var probability
+		if(myOptions.probability){
+			 probability = myOptions.probability;
+		}else{
+			probability = [10,2,1,1,1,1];
+		}
+		var notRandomNumber = [];
+		for(var key in myOptions.providers){
+			for (var i = 0; i < probability[key]; i++) {
+				notRandomNumber.push(key);
+			}
+		}
+		var idx = Math.floor(Math.random() * notRandomNumber.length);
+		if(myOptions.providers[notRandomNumber[idx]] == "Google"){
+			Logix360GeoTools.requestReverseGeoCodeGoogle(myOptions,lat,lng, function(res){
+			callback (res);
+			})
+		}else if(myOptions.providers[notRandomNumber[idx]] == "Bing"){
+			Logix360GeoTools.requestReverseGeoCodeBingMaps(myOptions,lat,lng, function(res){
+			callback (res);
+			})
+		}else if(myOptions.providers[notRandomNumber[idx]] == "OSM"){
+			Logix360GeoTools.requestReverseGeoCodeOSM(lat,lng, function(res){
+			callback (res);
+			})
+		}
+	},
+
+	requestReverseGeoCodeOSM: function(lat,lng,callback) {
+		if (lat.lat && lat.lng) {
+			callback = lng
+			lng = lat.lng
+			lat = lat.lat
+		};
+
+		var query = 'lat='+lat.toString() + '&lon=' + lng.toString() + '&zoom=18&addressdetails=1',
+				options = {
+					host : 'nominatim.openstreetmap.org',
+					path: '/reverse?format=json&' + query
+				};
+		var req = https.get(options, function(res) {
+			if(res.statusCode === 429){
+				callback(new Error ("OSM Returned..Too Many Request"));
+				return;
+			}
+			console.log('Response: ' + res.statusCode);
+			var results = '';
+			res.on('error', function(e) {
+				console.log('Got error: ' + e.message);
+			});
+			res.on('data', function(data) {
+				results += data;
+			});
+			res.on('end', function() {
+				var body = JSON.parse(results);
+				if (body.error_message) {
+					console.log(body.error_message);
+					return;
+				}
+				// var address = Logix360GeoTools.parseReverseGeoAddressGoogle(body)
+				var address = body;
+				if (callback) {
+					callback(address);
+				} else {
+					return address;
+				};
+			});
+		});
+
+
+	},
+
+	requestReverseGeoCodeBingMaps: function(myOptions,lat,lng,callback) {
+		if (lat.lat && lat.lng) {
+			callback = lng
+			lng = lat.lng
+			lat = lat.lat
+		};
+
+		var query = lat.toString() + ',' + lng.toString() + '?o=json&key='+myOptions.bingAPIKey,
+				options = {
+					host : 'dev.virtualearth.net',
+					path: '/REST/V1/Locations/' + query
+				};
+		var req = https.get(options, function(res) {
+			console.log('Response: ' + res.statusCode);
+			var results = '';
+			res.on('error', function(e) {
+				console.log('Got error: ' + e.message);
+			});
+			res.on('data', function(data) {
+				results += data;
+			});
+			res.on('end', function() {
+				var body = JSON.parse(results);
+				if (body.error_message) {
+					console.log(body.error_message);
+					return;
+				}
+				var address = Logix360GeoTools.parseReverseGeoBingResuts(body)
+				if (callback) {
+					callback(address);
+				} else {
+					return address;
+				};
+			});
+		});
+
+
+	},
+
+	parseReverseGeoBingResuts: function(data) {
+
+		var address = {
+			provider: "Bing",
+			full_address : data.resourceSets[0].resources[0].address.formattedAddress
+		}
+
+		if(data.resourceSets[0].resources[0].address.addressLine){
+			address.street_number = data.resourceSets[0].resources[0].address.addressLine;
+		}
+		if(data.resourceSets[0].resources[0].address.adminDistrict){
+			address.municipality = data.resourceSets[0].resources[0].address.adminDistrict;
+		}
+		if(data.resourceSets[0].resources[0].address.adminDistrict2){
+			address.city = data.resourceSets[0].resources[0].address.adminDistrict2;
+		}
+		if(data.resourceSets[0].resources[0].address.countryRegion){
+			address.country = data.resourceSets[0].resources[0].address.countryRegion;
+		}
+		if(data.resourceSets[0].resources[0].address.locality){
+			address.municipality2 = data.resourceSets[0].resources[0].address.locality;
+		}
+
+
+		return address;
+	},
+
+	requestReverseGeoCodeGoogle: function(myOptions,lat,lng,callback) {
+		if (lat.lat && lat.lng) {
+			callback = lng
+			lng = lat.lng
+			lat = lat.lat
+		};
+
+		var query = lat.toString() + ',' + lng.toString() + '&sensor=false',
+				options = {
+					host : 'maps.googleapis.com',
+					path: '/maps/api/geocode/json?latlng=' + query + '&key=' + myOptions.googleAPIKey
+				};
+		var req = https.get(options, function(res) {
+			console.log('Response: ' + res.statusCode);
+			var results = '';
+			res.on('error', function(e) {
+				console.log('Got error: ' + e.message);
+			});
+			res.on('data', function(data) {
+				results += data;
+			});
+			res.on('end', function() {
+				var body = JSON.parse(results);
+				if (body.error_message) {
+					console.log(body.error_message);
+					return;
+				}
+				var address = Logix360GeoTools.parseReverseGeoAddressGoogle(body)
+				if (callback) {
+					callback(address);
+				} else {
+					return address;
+				};
+			});
+		});
+
+
+	},
+
+	parseReverseGeoAddressGoogle: function(data){
+		var address = {
+			provider: "Google",
+			full_address : data.results[0].formatted_address
+		},
+		addressJSON = data.results[0].address_components;
+		for (var i = 0; i < addressJSON.length; i++) {
+			switch (addressJSON[i].types[0]) {
+				case 'street_number':
+					address.street_number = addressJSON[i].long_name;
+					break;
+				case 'route':
+					address.street = addressJSON[i].long_name;
+					break;
+				case 'neighborhood':
+					address.neighborhood = addressJSON[i].long_name;
+					break;
+				case 'locality' || 'sublocality':
+					address.city = addressJSON[i].long_name;
+					break;
+				case 'administrative_area_level_2':
+					address.municipality2 = addressJSON[i].long_name;
+					break;
+				case 'administrative_area_level_1':
+					address.municipality = addressJSON[i].long_name;
+					break;
+				case 'country':
+					address.country = addressJSON[i].long_name;
+					break;
+				case 'postal_code':
+					address.postal_code = addressJSON[i].long_name;
+					break;
+			};
+		};
+		return address;
+	},
+
 	computeDistanceInKm: function (lat1, lng1, lat2, lng2) {
 		//Check if point or Object.. if object with lat and lng assign to variable
 		if (lat1.lat && lng1.lat) {
